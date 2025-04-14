f92820ea3ef8639bab5523a5ba3c10468e2a5bc0
SamaraSR <146429344+SamaraSR@users.noreply.github.com>
2025-04-14 18:52:10 +0000

    update nivel aventureiro tema2

--- super_trunfo.c
+++ super_trunfo.c
@@ -9,7 +9,7 @@ int main() {
 	char estado1, estado2;
 	char codigo_da_carta1[5], codigo_da_carta2[5]; 
 	char nome_da_cidade1[50], nome_da_cidade2[50];	
-	int n_pontos_turisticos1, n_pontos_turisticos2, populacao1, populacao2;
+	int n_pontos_turisticos1, n_pontos_turisticos2, populacao1, populacao2, escolha;
 	float area1, area2, pib1, pib2, densidade_populacional1, densidade_populacional2, pib_per_capita1, pib_per_capita2, super_poder1, super_poder2, inverso_densidade1, inverso_densidade2 ;
 
 	printf("*** DESAFIO SUPER TRUNFO ***\n\n");
@@ -114,7 +114,90 @@ int main() {
 
     printf("\n\n\n");
     
-    //resultado das comparações 1 = verdadeiro e 0 = falso-------------------------
+    printf("*** Agora é hora do desafio! ***\n\n");
+    printf("Escolha qual dos atributos será usado para comparar as cartas:\n\n");
+    printf("1 - População.\n");
+    printf("2 - Área.\n");
+    printf("3 - PIB.\n");
+    printf("4 - Nº de Pontos Turísticos.\n");
+    printf("5 - Densidade demográfica\n\n");
+    printf("Escolha: ");
+    scanf("%d", &escolha);
+	    
+    printf("\n\n");
+    
+    
+    printf("Carta1: %s x Carta2: %s.\n", nome_da_cidade1, nome_da_cidade2);
+    
+    switch(escolha){
+    	
+    case 1:    	
+        printf("Atributo escolhido: População\n");
+        printf("Carta1: População %d  x Carta2: População %d .\n", populacao1, populacao2);
+        
+        if (populacao1 > populacao2)
+            printf("** Carta1 Venceu!! **\n");
+        else if (populacao1 < populacao2)
+            printf("** Carta2 Venceu!! **\n");
+        else
+            printf("** Empate!! **\n");
+        break;
+
+    case 2:    	
+    	printf("Atributo escolhido: Área\n");
+    	printf("Carta1: área %.2f km² x Carta2: área %.2f km².\n", area1, area2);
+    	
+        if (area1 > area2)
+            printf("** Carta1 Venceu!! **\n");
+        else if (area1 < area2)
+            printf("** Carta2 Venceu!! **\n");
+        else
+            printf("** Empate!! **\n");
+        break;
+
+    case 3:    	
+        printf("Atributo escolhido: PIB\n");
+        printf("Carta1: PIB %.2f bilhões de reais x Carta2: PIB %.2f bilhões de reais\n", pib1, pib2);
+        
+        if (pib1 > pib2)
+            printf("** Carta1 Venceu!! **\n");
+        else if (pib1 < pib2)
+            printf("** Carta2 Venceu!! **\n");
+        else
+            printf("** Empate!! **\n");
+        break;
+
+    case 4:    	
+        printf("Atributo escolhido: Nº de Pontos Turísticos\n");
+        printf("Carta1: %d x Carta2: %d.\n", n_pontos_turisticos1, n_pontos_turisticos2);
+        
+        if (n_pontos_turisticos1 > n_pontos_turisticos2)
+            printf("** Carta1 Venceu!! **\n");
+        else if (n_pontos_turisticos1 < n_pontos_turisticos2)
+            printf("** Carta2 Venceu!! **\n");
+        else
+            printf("** Empate!! **\n");
+        break;
+
+    case 5:    	
+        printf("Atributo escolhido: Densidade demográfica\n");
+        printf("Carta1:  %.2f hab/km² x  %.2f hab/km²\n", densidade_populacional1, densidade_populacional2);
+        
+        if (densidade_populacional1 < densidade_populacional2)
+            printf("** Carta1 Venceu!! **\n");
+        else if (densidade_populacional1 > densidade_populacional2)
+            printf("** Carta2 Venceu!! **\n");
+        else
+            printf("** Empate!! **\n");
+        break;
+
+    default:
+        printf("Escolha inválida.\n");
+        break;
+}
+   
+    
+    /*resultado das comparações 1 = verdadeiro e 0 = falso-------------------------
     
     printf("*** RESULTADO ***\n\n");
     printf("Comparação de Cartas:\n\n");
@@ -140,7 +223,7 @@ int main() {
 		printf("Carta 1 - %s: %d\n", nome_da_cidade1, n_pontos_turisticos1);
     	printf("Carta 2 - %s: %d\n\n", nome_da_cidade2, n_pontos_turisticos2);
     	printf("Resultado: Carta 2 - (%s) venceu!\n", nome_da_cidade2);
-	}
+	}*/
 
 	return 0;
 }
